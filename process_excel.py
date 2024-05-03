5607dedc32f86318d3448f2ab780a33ec89a1ebe
FlamurMaliqi <120040061+FlamurMaliqi@users.noreply.github.com>
2024-05-03 17:40:11 +0200

    path creation

--- process_excel.py
+++ process_excel.py
@@ -9,10 +9,17 @@ workbook = openpyxl.load_workbook(file_path)
 # Wähle das erste Arbeitsblatt aus
 worksheet = workbook["Übersicht-Overview"]
 
+relativ_path = 'test-dictionary/'
+
 # Iteriere über die Zellen im Arbeitsblatt
-for row in worksheet.iter_rows(min_row=8, max_row=15):
+for row in worksheet.iter_rows(min_row=7):
     cell = row[0]
     # Überprüfe, ob die Zelle einen Hyperlink enthält
+    father_path = ''
+    if cell.value:
+        if '. ' in cell.value:
+            father_path = 'Band_' + cell.value.replace('. ', '-').replace(' ', '_').replace('&', 'und') 
+        
    
 
 # Schließe die Excel-Datei
