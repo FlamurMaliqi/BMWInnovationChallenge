ea7582126fcbd8a18d6d3a8c423ac63442465ce9
FlamurMaliqi <120040061+FlamurMaliqi@users.noreply.github.com>
2024-05-03 20:09:57 +0200

    feat: Add function to process Excel file and extract paths
    The code changes introduce a new function `process_excel` that takes a file path as input and processes the Excel file. It selects the first worksheet, iterates over the rows starting from the 7th row, and checks if a cell contains a hyperlink. If it does, it constructs a path based on the cell value and appends it to a list of paths. The function returns the list of paths.

--- process_excel.py
+++ process_excel.py
@@ -3,24 +3,33 @@ import openpyxl
 # Pfad zur Excel-Datei
 file_path = 'test_file.xlsx'
 
-# Öffne die Excel-Datei
-workbook = openpyxl.load_workbook(file_path)
+def process_excel(file_path):
+    # Öffne die Excel-Datei
+    workbook = openpyxl.load_workbook(file_path)
 
-# Wähle das erste Arbeitsblatt aus
-worksheet = workbook["Übersicht-Overview"]
+    # Wähle das erste Arbeitsblatt aus
+    worksheet = workbook["Übersicht-Overview"]
 
-relativ_path = 'test-dictionary/'
+    relativ_path = 'test-dictionary/'
+    list_of_paths = []
 
-# Iteriere über die Zellen im Arbeitsblatt
-for row in worksheet.iter_rows(min_row=7):
-    cell = row[0]
-    # Überprüfe, ob die Zelle einen Hyperlink enthält
     father_path = ''
-    if cell.value:
-        if '. ' in cell.value:
-            father_path = 'Band_' + cell.value.replace('. ', '-').replace(' ', '_').replace('&', 'und') 
-        
-   
+    # Iteriere über die Zellen im Arbeitsblatt
+    for row in worksheet.iter_rows(min_row=7):
+        cell = row[0]
+        if cell.value == "Template":
+            break
+        # Überprüfe, ob die Zelle einen Hyperlink enthält
+        if cell.value:
+            if '. ' in cell.value:
+                father_path = 'Band_' + cell.value.replace('. ', '-').replace(' ', '_').replace('&', 'und') 
+                continue
+            path = father_path + '/' + cell.value.replace(' ', '_')
+            list_of_paths.append(path)
 
-# Schließe die Excel-Datei
-workbook.close()
+    workbook.close()
+    return list_of_paths
+            
+    # Schließe die Excel-Datei
+
+print(process_excel(file_path))
